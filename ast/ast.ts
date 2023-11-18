0b08b7be44ca9576ce7a159f2cb3238158b13d59
MoonSupport <jiwon3346@naver.com>
2023-11-18 11:38:13 +0900

    fix: ast 멘탈 모델 정리

--- ast/ast.ts
+++ ast/ast.ts
@@ -1,12 +1,5 @@
 import { Token } from "../token";
 
-// 상태란 무엇인가
-// 표현식과 또 다른 상태, 식별자로 구성되어 있음
-// 표현식이란 무엇인가
-//   연산 가능한 값. 문자열, 숫자, 그 외 여러 연산자
-// 식별자란 상태도 표현식도 아닌 것인가
-// 값을 담아두는 ( 표현식을 담아둔다고도 볼 수 있나? ) 메모리를 참조하는 이름
-
 type NodeType = "LetStatement" | "IntegerLiteral" | "Identifier";
 
 export interface Node {
@@ -17,42 +10,43 @@ export interface Node {
 
 export interface StatementNode extends Node {}
 
-interface LetStatement {
+interface LetStatementRequirement {
   token: Token;
-  name: IdentifierNode;
-  value: ExpressionNode;
+  name: Identifier;
+  value: Expression;
 }
 
-interface Identifier {
+interface IdentifierRequirement {
   token: Token;
   value: string;
 }
 
-interface IdentifierNode extends Node {}
+interface Identifier extends Node {}
 
-export const createIdentifider = ({ token, value }: Identifier): Node => ({
+export const createIdentifider = ({ token, value }: IdentifierRequirement): Node => ({
   _type: "Identifier",
   tokenLiteral: () => token.literal,
   toString: () => value,
 });
 
-export const createLetStatement = ({ token, name, value }: LetStatement): StatementNode => ({
+export const createLetStatement = ({ token, name, value }: LetStatementRequirement): StatementNode => ({
   _type: "LetStatement",
   tokenLiteral: () => token.literal,
   toString: () => `${token.literal} ${name.toString()} = ${value.toString()}`,
 });
 
-interface IntegerExpression {
+interface IntegerLiteralRequirement {
   token: Token;
   value: number;
 }
-export interface ExpressionNode extends Node {}
 
-export interface IntegerLiteral extends ExpressionNode {
+export interface Expression extends Node {}
+
+export interface IntegerLiteral extends Node {
   value: number;
 }
 
-export const createIntegerExpression = ({ token, value }: IntegerExpression): IntegerLiteral => ({
+export const createIntegerExpression = ({ token, value }: IntegerLiteralRequirement): IntegerLiteral => ({
   _type: "IntegerLiteral",
   value,
   tokenLiteral: () => token.literal,
